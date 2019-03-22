bbbb6a4aad7a3fac32cc8e2679bdab920966d335
jhbarkley <iamtheprophecy27@yahoo.com>
2019-03-21 21:08:44 -0400

    got dice to roll,fixed buttons

--- catch100.js
+++ catch100.js
@@ -1,17 +1,29 @@
 
 
 
-    let rollDiceButton = document.getElementsByClassName("diceRoll");
-    let firstDice = document.getElementsByClassName('firstDice');
+    let rollDiceButton = document.getElementById('mee');
+    let firstDice = document.getElementById('firstDice');
     let secondDice = document.getElementsByClassName('secondDice');
     
-    
-    
-    
-    
-    rollDiceButton.addEventListener("click", function(){
+    let images =["dice1.png","dice2.png","dice3.png","dice4.png","dice5.png","dice6.png", ]
+
+
+
+    function roll() {
         alert("Hey");
+    }
+    
+    
+    rollDiceButton.addEventListener('click', function(){
+        let random = Math.floor((Math.random() * 5) );
+        firstDice.src = images[random];
+        secondDice.src = images[random];
+        
     });
+
+    
+    
+
 
     function diceChange(diceroll){
         let random = Math.floor((Math.random() * 6) + 1);
