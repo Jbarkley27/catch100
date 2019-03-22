1edf51613aee589983d4625cae0a13b76f672ce8
jhbarkley <iamtheprophecy27@yahoo.com>
2019-03-22 13:22:04 -0400

    still need to fix logic issue

--- catch100.js
+++ catch100.js
@@ -7,7 +7,7 @@
     let addOneButton = document.getElementById('addOne');
     let score = document.getElementById('score');
     let stepDisplay = document.getElementById('stepDisplay');
-
+    let resetButton = document.getElementById('reset');
 
     let counter = 0;
     let stepCounter = 0;
@@ -18,6 +18,7 @@
     let images =["placeholder","dice1.png","dice2.png","dice3.png","dice4.png","dice5.png","dice6.png", ]
 
     let lastDigit = counter%10;
+
     //THIS EVENT ROLLS DICE AT RANDOM AND DISPLAYS THEM
     rollDiceButton.addEventListener('click', function(){
        diceChange(firstDice);
@@ -26,7 +27,7 @@
        score.innerText = counter;
        stepCounter++;
        stepDisplay.innerText=`Steps = ${stepCounter}`;
-       counterss(counter);
+       
        
        
     });
@@ -34,36 +35,36 @@
 
    addOneButton.addEventListener("click", function(){
        addOne();
-       
-       
-   })
+  });
     
-   
+   resetButton.addEventListener("click", reset);
 
     //FUNCTION FOR ROLLING THE DICE
     function diceChange(diceNum){
-        currentRoll = 0;
+        
         let random = Math.floor((Math.random() * 6) +1);
         diceNum.src = images[random];
         counter = counter + random;
-        currentRoll = currentRoll + random;
-        
-        
+        currentRoll = currentRoll + counter;
         score.innerText = counter;
         
-
+        currentRoll = 0;
         
     }
     
-    
+    //ADD ONE BUTTON
     function addOne(){
         counter++;
+        currentRoll=+counter;
         counterss();
         score.innerText = counter;
         stepCounter++;
         stepDisplay.innerText=`Steps = ${stepCounter}`;
+        currentRoll = 0;
     }
     
+    //LOGIC OF GAME SCORING
+    //
     function counterss(){
         if(counter > 100){
            counter = counter - 100;
@@ -72,9 +73,19 @@
         } else if (lastDigit === 5) {
             counter = counter + 5;
             score.innerText = counter;
+
         } else if (counter % 13 === 0) {
-            counter = currentRoll * 2 + counter;
-            score.innerText = counter;
+            counter = ((currentRoll * 2 + counter)-counter)-1;
+            score.innerText = counter ;
         }
         
+    }
+
+    //RESETS GAME SCORE
+    function reset (){
+        counter = 0;
+        score.innerText = 0;
+        stepCounter = 0;
+        stepDisplay.innerText=`Steps = 0`;
+        currentRoll = 0;
     }
