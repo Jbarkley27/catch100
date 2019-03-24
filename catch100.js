10f500d43ba873035acd4d74f5d8ea80d1a347ab
jhbarkley <iamtheprophecy27@yahoo.com>
2019-03-23 23:53:44 -0400

    fixed text highlight

--- catch100.js
+++ catch100.js
@@ -9,24 +9,27 @@
     let stepDisplay = document.getElementById('stepDisplay');
     let resetButton = document.getElementById('reset');
 
+    let ruleOne1 = document.getElementById('rule1');
+    let ruleTwo2 = document.getElementById('rule2');
+    let ruleThree3 = document.getElementById('rule3');
+
     let counter = 0;
     let stepCounter = 0;
     let currentRoll = 0;
    
-    
+    let lastDigit;
+    let lastDigit2;
     //ARRAY OF DICE IMAGES
     let images =["placeholder","dice1.png","dice2.png","dice3.png","dice4.png","dice5.png","dice6.png", ]
 
-    let lastDigit = counter%10;
+    
 
     //THIS EVENT ROLLS DICE AT RANDOM AND DISPLAYS THEM
     rollDiceButton.addEventListener('click', function(){
-       diceChange(firstDice);
-       diceChange(secondDice);
-       
-       score.innerText = counter;
+       diceChange();
        stepCounter++;
        stepDisplay.innerText=`Steps = ${stepCounter}`;
+       
        
        
        
@@ -35,29 +38,101 @@
 
    addOneButton.addEventListener("click", function(){
        addOne();
+
   });
     
    resetButton.addEventListener("click", reset);
 
+
+  function removeBoldRules(){
+    ruleOne1.classList.remove('boldText');
+    ruleTwo2.classList.remove('boldText');
+    ruleThree3.classList.remove('boldText');
+  }
+
+
+
+
     //FUNCTION FOR ROLLING THE DICE
-    function diceChange(diceNum){
+    function diceChange(){
+
+        removeBoldRules();
+        let diceAddition = 0;
         
         let random = Math.floor((Math.random() * 6) +1);
-        diceNum.src = images[random];
-        counter = counter + random;
-        currentRoll = currentRoll + counter;
-        score.innerText = counter;
+        diceAddition += random;
+        firstDice.src = images[random];
+
+        let random2 = Math.floor((Math.random() * 6) +1);
+        diceAddition += random2;
+        secondDice.src = images[random2];
+
+        counter +=diceAddition;
+        lastDigit = counter%10;
+
+        if(counter > 100){
+            counter = counter - 100;
+            score.innerText = counter;
+            ruleThree3.classList.add('boldText');
+            
+
+             
+         } else if (lastDigit === 5) {
+             counter = counter + 5;
+             score.innerText = counter;
+             ruleOne1.classList.add('boldText');
+             
+ 
+         } else if (counter % 13 === 0) {
+             counter -= diceAddition *2;
+             score.innerText = counter ;
+             ruleTwo2.classList.add('boldText');
+             
+         } else {
+            score.innerText = counter;
+         }
+
+        testForWin(counter);
         
-        currentRoll = 0;
+        
+        
+        diceAddition = 0;
+        lastDigit = 0;
         
     }
     
     //ADD ONE BUTTON
     function addOne(){
         counter++;
-        currentRoll=+counter;
-        counterss();
-        score.innerText = counter;
+        removeBoldRules();
+        
+        lastDigit2 = counter%10;
+        if(counter > 100){
+            counter = counter - 100;
+            score.innerText = counter;
+            ruleThree3.classList.add('boldText');
+             
+         } else if (lastDigit2 === 5) {
+             counter = counter + 5;
+             score.innerText = counter;
+             ruleOne1.classList.add('boldText');
+ 
+         } else if (counter % 13 === 0) {
+             counter -= 2 ;
+             score.innerText = counter ;
+             ruleTwo2.classList.add('boldText');
+         } else {
+            score.innerText = counter;
+            
+         }
+
+         score.innerText = counter;
+
+         testForWin(counter);
+
+        lastDigit2 = 0;
+
+        
         stepCounter++;
         stepDisplay.innerText=`Steps = ${stepCounter}`;
         currentRoll = 0;
@@ -65,21 +140,7 @@
     
     //LOGIC OF GAME SCORING
     //
-    function counterss(){
-        if(counter > 100){
-           counter = counter - 100;
-           score.innerText = counter;
-            
-        } else if (lastDigit === 5) {
-            counter = counter + 5;
-            score.innerText = counter;
-
-        } else if (counter % 13 === 0) {
-            counter = ((currentRoll * 2 + counter)-counter)-1;
-            score.innerText = counter ;
-        }
-        
-    }
+    
 
     //RESETS GAME SCORE
     function reset (){
@@ -88,4 +149,20 @@
         stepCounter = 0;
         stepDisplay.innerText=`Steps = 0`;
         currentRoll = 0;
+
+        removeBoldRules();
+
+        firstDice.src = images[1];
+        secondDice.src = images[1];
+
+        addOneButton.classList.remove('hide');
+        rollDiceButton.classList.remove('hide');
     }
+
+    function testForWin(num){
+        if(num === 100) {
+            score.innerText = 'YOU WIN!!';
+            addOneButton.classList.add('hide');
+            rollDiceButton.classList.add('hide');
+    }
+}
